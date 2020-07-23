cddfa268cc79a975d0292f0681cec9453476c838
guoyuncong <guoyuncong@yagena.net>
2020-07-23 14:55:21 +0800

    feat: 项目启动时执行

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 SpringBoot 整合
 
+global-exception    全局异常处理
+graylog-logback     graylog 日志聚合工具
+spring-security     spring security 基础
+startup-execute     项目启动执行
+web-cors            CORS 支持（跨域）
+
---
+++ startup-execute/src/main/java/com/springboot/startup/StartupExecuteApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.startup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartupExecuteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StartupExecuteApplication.class, args);
+    }
+
+}
