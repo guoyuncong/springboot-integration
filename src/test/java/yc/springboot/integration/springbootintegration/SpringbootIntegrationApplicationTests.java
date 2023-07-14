9a1167c3fa96f6bbfdf7ffd6d93ca97c7ecc9e7d
guoyuncong <guoyuncong@lazbao.com>
2023-07-14 15:34:32 +0800

    init

---
+++ src/test/java/yc/springboot/integration/springbootintegration/SpringbootIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package yc.springboot.integration.springbootintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
