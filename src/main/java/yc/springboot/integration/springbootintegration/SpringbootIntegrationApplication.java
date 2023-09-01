14ac12fe9c06d0b71ac71e4a089a9093f2690dd5
guoyuncong <guoyuncong@lazbao.com>
2023-09-01 11:00:03 +0800

    feat: init

--- src/main/java/yc/springboot/integration/springbootintegration/SpringbootIntegrationApplication.java
+++ src/main/java/yc/springboot/integration/springbootintegration/SpringbootIntegrationApplication.java
@@ -1,7 +1,11 @@
 package yc.springboot.integration.springbootintegration;
 
+import com.alibaba.fastjson.JSON;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.List;
+
 
 @SpringBootApplication
 public class SpringbootIntegrationApplication {
--- src/test/java/yc/springboot/integration/springbootintegration/SpringbootIntegrationApplicationTests.java
+++ src/test/java/yc/springboot/integration/springbootintegration/SpringbootIntegrationApplicationTests.java
@@ -1,13 +1,19 @@
 package yc.springboot.integration.springbootintegration;
 
+import com.alibaba.fastjson.JSON;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest
 class SpringbootIntegrationApplicationTests {
 
 	@Test
 	void contextLoads() {
+		String logisticsAcceptStatus = "[\"ACCEPT\",\"TRANSPORT\",\"SIGN\", NULL]";
+		List<String> logisticsAcceptStatusList = JSON.parseArray(logisticsAcceptStatus, String.class);
+		System.out.println(logisticsAcceptStatusList);
 	}
 
 }
